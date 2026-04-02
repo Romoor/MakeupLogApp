9d5bcf3546c2191a15e507aa668cd38940cfbfc2
Romoor <74839554+Romoor@users.noreply.github.com>
2026-04-01 17:36:00 -0700

    initialized first part of project (scraping whole website) following tutorial for scrapy

--- test.py
+++ test.py
@@ -1,1 +1,23 @@
-hello
+#interact with website like person?
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import pandas as pd
+
+#get data from page?
+from bs4 import BeautifulSoup
+import requests
+
+cService = webdriver.ChromeService(executable_path='Users/roseromo/Downloads/chromedriver-mac-x64')
+
+driver = webdriver.Chrome(service=cService)
+
+""" url = 'https://www.ulta.com/'
+
+page = requests.get(url)
+
+soup = BeautifulSoup(page.text, 'html')
+
+print(soup) """
+
+driver.get('https://hoopshype.com/salaries/players/')
